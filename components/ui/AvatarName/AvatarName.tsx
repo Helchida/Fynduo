bde541784595433bb30e5167fb2410137adc3916
Morgan Leclercq <morgan.leclercq89@gmail.com>
2026-01-04 23:58:39 +0100

    [FIX] Fallback Avatar si nom vide

--- components/ui/AvatarName/AvatarName.tsx
+++ components/ui/AvatarName/AvatarName.tsx
@@ -1,14 +1,20 @@
 import { View, Text } from "react-native";
 import { styles } from "./AvatarName.style";
 import { AvatarNameProps } from "./AvatarName.type";
+import { User } from "lucide-react-native";
 
-const AvatarName: React.FC<AvatarNameProps> = ({name}) => {
-    const initialName = name.charAt(0).toUpperCase();
-    return (
-        <View style={styles.avatarPlaceholder}>
-            <Text style={styles.avatarText}>{initialName}</Text>
-        </View>
-    );
-}
+const AvatarName: React.FC<AvatarNameProps> = ({ name }) => {
+  const hasName = name && name.trim().length > 0;
+  const initialName = hasName ? name.trim().charAt(0).toUpperCase() : null;
+  return (
+    <View style={styles.avatarPlaceholder}>
+      {hasName ? (
+        <Text style={styles.avatarText}>{initialName}</Text>
+      ) : (
+        <User color="#fff" size={24} strokeWidth={2.5} />
+      )}
+    </View>
+  );
+};
 
 export default AvatarName;
